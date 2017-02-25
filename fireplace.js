ac31dd35c4210a16d7abc11d7f213464df972b0d
Robert <robert.balicki@gmail.com>
2017-02-25 15:52:41 -0500

    moar testing

--- fireplace.js
+++ fireplace.js
@@ -5,24 +5,43 @@ import Equation from './Equation';
 // base frame works correctly
 
 const {
-  FUNDAMENTALS,
+  FUNDAMENTALS: {
+    inch,
+    topw,
+    totbt
+  },
   BASE
 } = measurements;
 
+// BASE FRAME
 const BASE_WIDTH_AT_FRAME = Equation.fromEquations(
-  FUNDAMENTALS.topw,
-  FUNDAMENTALS.topw,
+  topw,
+  topw,
   BASE.FRAME.longSide,
-  FUNDAMENTALS.totbt,
-  FUNDAMENTALS.totbt
+  totbt,
+  totbt
 );
 assert(BASE.OUTER.WIDTH.isEqual(BASE_WIDTH_AT_FRAME), 'Outer width does not match up');
 
 const BASE_DEPTH_AT_FRAME = Equation.fromEquations(
-  FUNDAMENTALS.topw,
-  FUNDAMENTALS.totbt, // support or long bar
-  FUNDAMENTALS.totbt,
+  topw,
+  totbt, // support or long bar
+  totbt,
   BASE.FRAME.shortSide
 );
-assert(BASE.OUTER.ACTUAL_DEPTH.isEqual(BASE_DEPTH_AT_FRAME), 'Actual depth does not match up');
+assert(BASE.OUTER.BOTTOM_DEPTH.isEqual(BASE_DEPTH_AT_FRAME), 'Actual depth does not match up');
+
+const BASE_DEPTH_AT_PANEL_BOTTOM = Equation.fromEquations(
+  topw,
+  BASE.PANELS.SIDE.widthBottom
+);
+assert(BASE.OUTER.BOTTOM_DEPTH.isEqual(BASE_DEPTH_AT_PANEL_BOTTOM), 'Side base panel at bottom does not match');
+
+const BASE_DEPTH_AT_PANEL_TOP = Equation.fromEquations(
+  topw,
+  BASE.PANELS.SIDE.widthTop
+);
+assert(BASE.OUTER.TOP_DEPTH.isEqual(BASE_DEPTH_AT_PANEL_TOP), 'Side base panel panel at top does not match');
+
+// BASE TOP doesn't need testing...
 
--- measurements.js
+++ measurements.js
@@ -1,39 +1,59 @@
 import units from './units';
 import Equation from './Equation';
 
+const { topw, totbt, inch } = units;
+
 // BASE measurements do not include the larger plywood on top, just the box
 // bounded by the plywood below
-const BASE_WIDTH = units.inch.setValue(58);
-const BASE_HEIGHT = units.inch.setValue(6);
-const BASE_CUTOUT_DEPTH = units.inch.setValue(0.5);
-const BASE_TOTAL_DEPTH = units.inch.setValue(26);
-const BASE_ACTUAL_DEPTH = BASE_TOTAL_DEPTH.subtract(BASE_CUTOUT_DEPTH);
+const BASE_WIDTH = inch.setValue(58);
+const BASE_HEIGHT = inch.setValue(6);
+const BASE_CUTOUT_DEPTH = inch.setValue(0.5);
+const BASE_TOP_DEPTH = inch.setValue(26);
+const BASE_BOTTOM_DEPTH = BASE_TOP_DEPTH.subtract(BASE_CUTOUT_DEPTH);
+
 
 export default {
   FUNDAMENTALS: {
-    topw: new Equation(units.topw),
-    totbt: new Equation(units.totbt),
-    inch: new Equation(units.inch),
+    topw: new Equation(topw),
+    totbt: new Equation(totbt),
+    inch: new Equation(inch),
   },
   BASE: {
     OUTER: {
       WIDTH: new Equation(BASE_WIDTH),
       HEIGHT: new Equation(BASE_HEIGHT),
-      TOTAL_DEPTH: new Equation(BASE_TOTAL_DEPTH),
-      ACTUAL_DEPTH: new Equation(BASE_ACTUAL_DEPTH),
+      TOP_DEPTH: new Equation(BASE_TOP_DEPTH),
+      BOTTOM_DEPTH: new Equation(BASE_BOTTOM_DEPTH),
     },
     FRAME: {
       support: new Equation(BASE_HEIGHT),
       shortSide: new Equation(
-        BASE_ACTUAL_DEPTH,
-        units.topw.multiply(-1),
-        units.totbt.multiply(-2)
+        BASE_BOTTOM_DEPTH,
+        topw.multiply(-1),
+        totbt.multiply(-2)
       ),
       longSide: new Equation(
         BASE_WIDTH,
-        units.topw.multiply(-2),
-        units.totbt.multiply(-2)
+        topw.multiply(-2),
+        totbt.multiply(-2)
       ),
+    },
+    PANELS: {
+      FRONT: {
+        width: new Equation(BASE_WIDTH),
+        height: new Equation(BASE_HEIGHT),
+      },
+      SIDE: {
+        widthTop: new Equation(
+          BASE_TOP_DEPTH,
+          topw.multiply(-1)
+        ),
+        widthBottom: new Equation(
+          BASE_BOTTOM_DEPTH,
+          topw.multiply(-1)
+        ),
+        height: new Equation(BASE_HEIGHT),
+      },
     },
   },
 }
